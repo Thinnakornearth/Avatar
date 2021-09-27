6dd625d0c7a88a7a98eb4ddcf5660198859f7e79
Joanna LAW <joannalaw801@gmail.com>
2021-09-27 20:14:04 +1000

    add datebase

--- travel-exemption/app.py
+++ travel-exemption/app.py
@@ -1,20 +1,44 @@
-from flask import Flask, request
-
+from flask import Flask, request, jsonify
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:12345@localhost:3306/travel_exemption"
 applications = []
+migrate = Migrate(app,db)
 
-@app.post("/")  
+
+class Application(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50),nullable = False )
+    visa_num = db.Column(db.String(15), nullable = False)
+    passport_num = db.Column(db.String(15), nullable = False)
+    reason = db.Column(db.String(200), nullable = False)
+    dob = db.Column(db.DateTime, nullable = False)
+    tel = db.Column(db.String(12),nullable = False)
+    attachment = db.Column(db.String(50),nullable = True)
+    sub_date = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    status = db.Column(db.String(15),nullable = False, default = "submitted")
+
+
+
+@app.post("/createapp")  
 def addApplication():
     newForm = request.get_json()
-    applications.append(newForm)
-    return newForm
+    newApplication = Application(**newForm)
+    db.session.add(newApplication)
+    db.session.commit()
+    return {
+        "id": newApplication.id,
+        "submitted at": newApplication.sub_date
+    }
 
 
 
-@app.get("/checkStatus/<a_id>")
+@app.get("/checkstatus/<a_id>")
 def checkStatus(a_id):
     for each in applications:
         if each["application_id"] == a_id:
@@ -22,7 +46,7 @@ def checkStatus(a_id):
     return "not exist"
 
 
-@app.get("/")
+@app.get("/retrieveall")
 def test():
     return {
         "applications" : applications
